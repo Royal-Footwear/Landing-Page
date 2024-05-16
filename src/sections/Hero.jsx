82113c8b1244d0f3979c8a56a2946201f0f4daee
Ben Vilakazi <116912017+BenVilakazi@users.noreply.github.com>
2024-05-16 13:32:39 +0200

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
           your active life.
         </p>
 
-        <Button label='Shop now' iconURL={arrowRight} />
+        <Button label='Shop now' iconURL={arrowRight} href={href} />
 
         <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
           {statistics.map((stat, index) => (
